9d867f2bcf587d05738b311144ff0d09d148082e
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-01-10 10:57:21 -0800

    SwiftUI Essentials: Handling user input   Section 2 Filter the list view Section 3 Add a control to toggle the state

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Tatsuya Moriguchi on 1/22/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill": "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow: .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
